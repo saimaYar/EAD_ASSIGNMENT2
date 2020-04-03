0bb183ff3ebd9c07ed705a3e1108f8da08dbb9f7
saimaYar <61507198+saimaYar@users.noreply.github.com>
2020-04-03 23:34:15 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace PRACTICE_EAD
 {
@@ -14,6 +15,16 @@ namespace PRACTICE_EAD
         [STAThread]
         static void Main()
         {
+            DAL obj = new DAL();
+            obj.save("saima 2", DateTime.Now);
+            obj.update(2, "new user", DateTime.Now);
+            obj.delete(2);
+            obj.ReadAndPrint();
+
+
+
+            System.Console.ReadKey();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
