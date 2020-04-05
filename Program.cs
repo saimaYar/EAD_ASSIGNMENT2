755975ca84aeedecc8d178e6f124bdd114b159e5
saimaYar <61507198+saimaYar@users.noreply.github.com>
2020-04-05 22:55:18 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,38 +16,24 @@ namespace PRACTICE_EAD
         static void Main()
         {
             DAL obj = new DAL();
-            userDTO dto = new userDTO();
+            var dto = new PMS.entities.userDTO();
             dto.id = 1;
             dto.name = "saima";
             dto.login = "saima";
            dto.password = "123";
             dto.gender = 'F';
+            dto.image = "pakistan.jpg";
             dto.adress = "lahore";
             dto.age = 20;
             dto.nic = "123";
             dto.dob = DateTime.Now.AddYears(-33);
-            var id=obj.save2(dto);
-           
-
-            List<userDTO> list = obj.GetAllUser();
-            foreach(var dt in list)
-            {
-                Console.WriteLine("id is:{0}", dt.id);
-                Console.WriteLine("name is:{0}", dt.name);
-                Console.WriteLine("password is:{0}", dt.password);
-                Console.WriteLine("login is:{0}", dt.login);
-                Console.WriteLine("adress is:{0}", dt.adress);
-                Console.WriteLine("age is:{0}", dt.age);
-                Console.WriteLine("gender is:{0}", dt.gender);
-                Console.WriteLine("nic is:{0}", dt.nic);
-                Console.WriteLine("dob is:{0}", dt.dob.ToShortDateString());
-                
-                
-            }
-
-
-
-            System.Console.ReadKey();
+            dto.email = "saima@gmail.com";
+            // var id=obj.save2(dto);
+            PMS.BAL.userBO.save(dto);
+            var data=PMS.BAL.userBO.GetAllUser();
+           var del= PMS.BAL.userBO.delete(1);
+            var prod=PMS.BAL.userBO.GetUserById(1);
+             System.Console.ReadKey();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
