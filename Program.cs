60adc754a5ca479593262cddbb732a5be80e7569
saimaYar <61507198+saimaYar@users.noreply.github.com>
2020-04-05 20:13:39 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,10 +16,34 @@ namespace PRACTICE_EAD
         static void Main()
         {
             DAL obj = new DAL();
-            obj.save("saima 2", DateTime.Now);
-            obj.update(2, "new user", DateTime.Now);
-            obj.delete(2);
-            obj.ReadAndPrint();
+            userDTO dto = new userDTO();
+            dto.id = 1;
+            dto.name = "saima";
+            dto.login = "saima";
+           dto.password = "123";
+            dto.gender = 'F';
+            dto.adress = "lahore";
+            dto.age = 20;
+            dto.nic = "123";
+            dto.dob = DateTime.Now.AddYears(-33);
+            var id=obj.save2(dto);
+           
+
+            List<userDTO> list = obj.GetAllUser();
+            foreach(var dt in list)
+            {
+                Console.WriteLine("id is:{0}", dt.id);
+                Console.WriteLine("name is:{0}", dt.name);
+                Console.WriteLine("password is:{0}", dt.password);
+                Console.WriteLine("login is:{0}", dt.login);
+                Console.WriteLine("adress is:{0}", dt.adress);
+                Console.WriteLine("age is:{0}", dt.age);
+                Console.WriteLine("gender is:{0}", dt.gender);
+                Console.WriteLine("nic is:{0}", dt.nic);
+                Console.WriteLine("dob is:{0}", dt.dob.ToShortDateString());
+                
+                
+            }
 
 
 
