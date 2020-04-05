1da0150171d551b18d389677103dd60a94c6d1a9
saimaYar <61507198+saimaYar@users.noreply.github.com>
2020-04-05 23:48:22 +0500

    Update adminDTO.cs

--- adminDTO.cs
+++ adminDTO.cs
@@ -8,5 +8,9 @@ namespace PMS.entities
 {
     class adminDTO
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
     }
 }
